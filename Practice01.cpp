038fae22b6d18edb5e48d8fddb8694a679688413
Akkeem Tyrell <138136345+adonisja@users.noreply.github.com>
2023-09-24 19:46:25 +0000

    update

--- Practice01.cpp
+++ Practice01.cpp
@@ -1,18 +1,42 @@
 #include <iostream>
 #include <climits>
+#include <array>
 using namespace std;
 
 int main()
 {
-    /* this accepts a number and outputs it
-    int num1;
+    // this accepts a number and outputs it
+    float num[] = {0}, sum = 0, avg = 0;
+    int count = 0, numSize = 0, i = 0;
 
-    cout << "Hello, this is my first test\n";
-    cout << "Enter a number: ";
-    cin >> num1; 
-    cout << num1 << "\n";   */
+    //This will accept a list of numbers and find the average between them
+    cout << "Enter a list of numbers or 0 to end: ";
+    cin >> num[0];
+    
+    while (num[i] != 0)
+    {
+        sum = sum + num[i];
+        count = count +=1;
+        cout << "Enter the next number or 0 to end: ";
+        cin >> num[i];
+    } 
+   // cout << "The sum of the numbers is: " << sum << "\n"; 
+   // cout << "The count of the numbers is: " << count << "\n";    
+    avg = sum / count;
+   //cout << "The average of the numbers is: " << avg << "\n";
+    numSize = sizeof(num); //This calculates the size of the array
+    cout << "The size of the array is: " << (numSize - 1) << "\n";
+
+    cout << "The average of: \n";
+    for(int a = 0; a < numSize; a += 1)
+    {
+        cout << num[a] << ", ";
+    }
+    cout << "\nis: " << avg;
+
 
     //This will output the size of numerous datatypes
+    /*
     cout << "The size of int is: " << sizeof(int) << "\n";
     cout << "The size of double is: " << sizeof(double) << "\n";
     cout << "The size of long is: " << sizeof(long) << "\n";
@@ -21,4 +45,5 @@ int main()
     cout << "The size of bool is: " << sizeof(bool) << "\n";
     cout << "This will output the lowest possible value of an int variable: " << INT_MIN << "\n";
     cout << "This will output the highest possible value of an int variable: " << INT_MAX << "\n";
+    */
 }
