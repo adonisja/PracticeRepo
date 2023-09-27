7d6a64dc09b926927f8f8e44892bb542ab565d4b
Akkeem Tyrell <138136345+adonisja@users.noreply.github.com>
2023-09-27 05:14:03 +0000

    update

---
+++ Practice06.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <climits>
+#include <array>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    float num1, num2;
+    char operation;
+    cout << "Enter your first number: ";
+    cin >> num1;
+    cout << "Enter your second number: ";
+    cin >> num2;
+    cout << "Enter the operation your wish to do";
+    cout <<  ;
+
+    switch (operation)
+    {
+    case '+':
+        cout << "\n" << num1 << operation << num2 << "is: " << num1 + num2;
+        break;
+    
+    case '-':
+    
+    default:
+        break;
+    }
+    cin >> operation;
+    return 0;
+}
