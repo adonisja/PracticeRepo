cbc982d30a6f1bba8cb4e1829b8542ae28b2b718
Akkeem Tyrell <138136345+adonisja@users.noreply.github.com>
2023-09-27 22:48:16 +0000

    update

--- weightedSum.py
+++ weightedSum.py
@@ -38,22 +38,21 @@ lookupTable2 = {
 
 print("Enter the base you wish to convert to decimal")
 base = int(input("You may ONLY choose from base 2 (binary), base 4, base 8 (octal), or base 16 (hexadecimal): "))
-print("base = ",base)
-while base != 2 or base != 4 or base != 8 or base != 16:
-    base = int(input("Invalid input! You may ONLY choose from base 2 (binary), base 4, base 8 (octal), or base 16 (hexadecimal): "))
-    print("base = ",base)
+# Assume base is correct
+
 num = str(input("Enter the value you wish to convert to decimal: "))
 i = int(len(num) - 1)
 sum = 0
 
 
 while i >= 0:
-    if base != 16:
-        x:int = int(num[i])
-        sum = sum + x * 10**x
-    else:
-        x:int = int(lookupTable[num[i]])
-        sum = sum + lookupTable[x] * 10**x
+    if 0 <= i < len(num):
+        if base != 16:
+            x:int = int(num[i])
+            sum = sum + x * 10**x
+        else:
+            x:int = int(lookupTable[num[i]])
+            sum = sum + lookupTable[x] * 10**x
     i += 1
     x -= 1
 
