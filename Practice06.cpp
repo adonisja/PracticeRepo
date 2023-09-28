71df735c4f9fa08675952946730ba50736be94c5
Akkeem Tyrell <138136345+adonisja@users.noreply.github.com>
2023-09-28 03:20:29 +0000

    update

--- Practice06.cpp
+++ Practice06.cpp
@@ -2,6 +2,7 @@
 #include <climits>
 #include <array>
 #include <iomanip>
+#include <cmath>
 using namespace std;
 
 int main()
@@ -12,20 +13,44 @@ int main()
     cin >> num1;
     cout << "Enter your second number: ";
     cin >> num2;
-    cout << "Enter the operation your wish to do";
-    cout <<  ;
+    cout << "Enter the operation your wish to do: ";
+    cin >>  operation; 
 
     switch (operation)
     {
     case '+':
-        cout << "\n" << num1 << operation << num2 << "is: " << num1 + num2;
+        cout << "\n" << num1 << operation << num2 << "is: " << num1 + num2 << "\n";
         break;
     
     case '-':
+        cout << "\n" << num1 << operation << num2 << "is: " << num1 - num2 << "\n";
+        break;
+    case '*':
+        cout << "\n" << num1 << operation << num2 << "is: " << num1 * num2 << "\n";
+        break;
+    case '/':
+        cout << "\n" << num1 << operation << num2 << "is: " << num1 / num2 << "\n";
+        break;
+    case '%':
+        bool isNum1Int, isNum2Int;
+        isNum1Int = ((int)num1 == num1);
+        isNum2Int = ((int)num2 == num2);
+        if (isNum1Int && isNum2Int)
+        {
+            cout << num1 << operation << num2 << " = " << (int)num1%(int)num2;
+        }
+        else
+        {
+            cout << "This operation is not valid!" << "\n";
+        }
+        break;
+    case '^':
+        cout << num1 << operation << num2 << " = " << pow(num1, num2) << "\n";
+        break;
     
     default:
+        cout << "Not a valid operation";
         break;
     }
-    cin >> operation;
     return 0;
 }
