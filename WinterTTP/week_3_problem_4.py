0372393e70594d76ea95e7a3c6f3c9daac827cd3
Akkeem Tyrell <138136345+adonisja@users.noreply.github.com>
2024-01-08 22:23:40 +0000

    update

---
+++ WinterTTP/week_3_problem_4.py
@@ -0,0 +1,30 @@
+def mathInterpreter():
+    print("Please use spaces!")
+    mathProblem = input("Enter an arithmetic problem: \n")
+    list = mathProblem.split() # splits the user input into a list of variables
+    print(list)
+    val1 = float(list[0]) #assigns the first member of the list as a float to variable val1
+    arithmetic = list[1] #assigns the second member of the list as the arithmetic sign of the problem
+    val2 = float(list[2]) #assigns the third member of the list as a float to variable val2
+    #uses a switch-case function on the arithmeic variable to determine which arithmetic procedure to follow
+    match arithmetic:
+        case '+':
+            print(val1+val2)
+        case '-':
+            print(val1-val2)
+        case '*':
+            print(val1*val2)
+        case '/':
+            if val2 == 0: # outputs an error of if the denominator of a division is 0
+                print('denominator cannot be 0')
+            else:
+                print(f"{val1/val2:.02f}")
+        case '**':
+            print(val1**val2)
+        case _:
+            raise TypeError("Invalid Syntax")
+
+def main():
+    mathInterpreter()
+
+main()
